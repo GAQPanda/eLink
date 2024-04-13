755f659134080e66dc9c5d239c9f9e1736108542
Fat Panda <70119893+GAQPanda@users.noreply.github.com>
2024-04-13 22:38:30 +0800

    Update Main.py
    Update Main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import serial.tools.list_ports
 
 txw = "RX"
 gctxw = "RX"
+ser3 = "none"
+version = "Releases 1.2"
 
 
 # 定义一个线程，用于接收TX状态
@@ -33,24 +35,68 @@ def gcsm():
     while True:
         ser.write(ser2.read())
 
+
+def gcrts():
+    global gctxw
+    global ser3
+    global ser2
+    while True:
+        ser2read = ser2.read()
+        if ser2read == b'\x08':
+            ser3.setRTS(True)
+            gctxw = "TX"
+        elif ser2read == b'\x88':
+            ser3.setRTS(False)
+            gctxw = "RX"
+
+
+def gcdtr():
+    global gctxw
+    global ser3
+    global ser2
+    while True:
+        ser2read = ser2.read()
+        if ser2read == b'\x08':
+            ser3.setDTR(True)
+            gctxw = "TX"
+        elif ser2read == b'\x88':
+            ser3.setDTR(False)
+            gctxw = "RX"
+
+
+
 # 打印字体样例
 print("TIP DEMO")
 print("\033[0;32;40m[INFO]\033[0m ")
 print("\033[0;33;40m[WARN]\033[0m ")
 print("\033[0;31;40m[ERRO]\033[0m ")
+print("\033[0;34;40m[ERRO]\033[0m Version：", version)
 os.system('cls')
 
 t1 = threading.Thread(target=etx)
 t2 = threading.Thread(target=gcsm)
+t3 = threading.Thread(target=gcrts)
+t4 = threading.Thread(target=gcdtr)
 # 打印欢迎界面
+print("""
+
+_____________________        ______     ______________ 
+\_   _____|__    ___/       /  __  \   /  |  \______  |
+ |    __)   |    |  ______  >      <  /   |  |_  /    /
+ |     \    |    | /_____/ /   --   \/    ^   / /    / 
+ \___  /    |____|         \______  /\____   | /____/  
+     \/                           \/      |__|         
+
+""")
 print("\033[0;32;40m[INFO]\033[0m FT-847 Satellite CAT（Computer Aided Transceiver) Program")
 print("\033[0;32;40m[INFO]\033[0m Made/Updated by BG5CVT 2024-04-05")
-print("\033[0;32;40m[INFO]\033[0m Version : Releases V1.1")
+print("\033[0;32;40m[INFO]\033[0m Version :", version)
 print("\033[0;33;40m[WARN]\033[0m Only for Windows OS")
 print("\033[0;33;40m[WARN]\033[0m Only can control 144Mhz/430Mhz band")
 print("\033[0;33;40m[WARN]\033[0m Only for FT-847,DO NOT use for other radios!")
-print("\033[0;33;40m[WARN]\033[0m This program is only for personal use, and the author is not responsible for any consequences caused by "
-      "the use of this program.")
+print(
+    "\033[0;33;40m[WARN]\033[0m This program is only for personal use, and the author is not responsible for any consequences caused by "
+    "the use of this program.")
 time.sleep(5)
 
 # 创建虚拟串口
@@ -98,7 +144,7 @@ while True:
             print("\033[0;32;40m[INFO]\033[0m [5] 自动多普勒校正")
             print("\033[0;32;40m[INFO]\033[0m [6] Greencube模式")
             print("\033[0;32;40m[INFO]\033[0m [7] 退出")
-            choice = str(input("请选择："))
+            choice = str(input("\033[0;32;40m[INFO]\033[0m 请选择："))
             if choice == "7":
                 break
             elif choice == "1":
@@ -157,10 +203,13 @@ while True:
                         ser.write(bytes.fromhex("000000004E"))
                         os.system('cls')
                         lst = data.split()
-                        try:
-                            t1.start()
-                        except IndexError:
-                            pass
+                        tstart = False
+                        if tstart is False:
+                            try:
+                                t1.start()
+                                tstart = True
+                            except RuntimeError:
+                                pass
                         # 打印接收到的数据
                         try:
                             print("\033[0;32;40m[INFO]\033[0m 卫星名称：", lst[0].lstrip("SN"))
@@ -208,13 +257,14 @@ while True:
                         # 等待1秒
                         time.sleep(1)
             elif choice == "6":
+                tstart = False
                 input("\033[0;33;40m[WARN]\033[0m 请确保FT-847已经退出打开Split Mode RX-TX"
                       "由于电台限制，副频率需手动调整"
                       "短按Enter键以继续")
                 os.system('cls')
                 print("\033[0;33;40m[WARN]\033[0m 请在Com0Com中设置双通端口对")
                 gcrate = 57600
-                gccom = input("\033[0;33;40m[WARN]\033[0m 请输入其中一个串口，并在SoundModern的CAT设置里"
+                gccom = input("\033[0;33;40m[WARN]\033[0m 请输入其中一个串口，并在SoundModern的设置里"
                               "设置另一个串口:")
                 while True:
                     try:
@@ -228,6 +278,41 @@ while True:
                         input("\033[0;31;40m[ERRO]\033[0m 请确保FT-847侧串口设置正确"
                               "短按Enter键进行重新设置")
                         continue
+                print("\033[0;32;40m[INFO]\033[0m PTT激活方式：")
+                print("\033[0;32;40m[INFO]\033[0m [1] CAT")
+                print("\033[0;32;40m[INFO]\033[0m [2] RTS")
+                print("\033[0;32;40m[INFO]\033[0m [3] DTR")
+                GCTXW = input("\033[0;32;40m[INFO]\033[0m 请选择：")
+                os.system("cls")
+                if GCTXW == "2":
+                    while True:
+                        RTSser = input("\033[0;32;40m[INFO]\033[0m 请输入RTS端口：")
+                        RTSrate = int(input("\033[0;32;40m[INFO]\033[0m 请输入RTS波特率："))
+                        try:
+                            ser3 = serial.Serial(RTSser, RTSrate, timeout=0.1)
+                            ser3.setRTS(False)
+                            ser3.setDTR(False)
+                            break
+                        except:
+                            print("\033[0;31;40m[ERRO]\033[0m 串口打开失败")
+                            input("\033[0;31;40m[ERRO]\033[0m 请确保RTS串口设置正确"
+                                  "短按Enter键进行重新设置")
+                            continue
+                elif GCTXW == "3":
+                    while True:
+                        DTRser = input("\033[0;32;40m[INFO]\033[0m 请输入DTR端口：")
+                        DTRrate = int(input("\033[0;32;40m[INFO]\033[0m 请输入DTR波特率："))
+                        try:
+                            ser3 = serial.Serial(DTRser, DTRrate, timeout=0.1)
+                            ser3.setRTS(False)
+                            ser3.setDTR(False)
+                            break
+                        except:
+                            print("\033[0;31;40m[ERRO]\033[0m 串口打开失败")
+                            input("\033[0;31;40m[ERRO]\033[0m 请确保DTR串口设置正确"
+                                  "短按Enter键进行重新设置")
+
+
                 # 创建一个DDE客户端 gc
                 dde_client = dde.CreateServer()
 
@@ -261,10 +346,18 @@ while True:
                         ser.write(bytes.fromhex("000000008E"))
                         os.system('cls')
                         lst = data.split()
-                        try:
-                            t2.start()
-                        except:
-                            pass
+                        if GCTXW == "2":
+                            if tstart is False:
+                                t3.start()
+                                tstart = True
+                        elif GCTXW == "3":
+                            if tstart is False:
+                                t4.start()
+                                tstart = True
+                        else:
+                            if tstart is False:
+                                t2.start()
+                                tstart = True
                         # 打印接收到的数据 gc
                         try:
                             print("\033[0;32;40m[INFO]\033[0m 卫星名称：", lst[0].lstrip("SN"))
@@ -307,12 +400,47 @@ while True:
                             ser.write(bytes.fromhex("0100000007"))
                             print("\033[0;32;40m[INFO]\033[0m 正在进行多普勒校正")
                             print("\033[0;32;40m[INFO]\033[0m 目前状态：", gctxw)
-                        # 等待1秒 gc
-                        time.sleep(1)
+                        # 等待0.5秒
+                        time.sleep(0.3)
     elif choice == "3":
+        print("\033[0;32;40m[INFO]\033[0m PTT激活方式：")
+        print("\033[0;32;40m[INFO]\033[0m [1] CAT")
+        print("\033[0;32;40m[INFO]\033[0m [2] RTS")
+        print("\033[0;32;40m[INFO]\033[0m [3] DTR")
+        CTTXW = input("\033[0;32;40m[INFO]\033[0m 请选择：")
         # 发射控制子菜单
         Tx = 0
         os.system('cls')
+        if CTTXW == "2":
+            while True:
+                RTSser = input("\033[0;32;40m[INFO]\033[0m 请输入RTS端口：")
+                RTSrate = int(input("\033[0;32;40m[INFO]\033[0m 请输入RTS波特率："))
+                try:
+                    ser3 = serial.Serial(RTSser, RTSrate, timeout=0.5)
+                    ser3.setRTS(False)
+                    ser3.setDTR(False)
+                    break
+                except:
+                    print("\033[0;31;40m[ERRO]\033[0m 串口打开失败")
+                    input("\033[0;31;40m[ERRO]\033[0m 请确保RTS串口设置正确"
+                          "短按Enter键进行重新设置")
+                    continue
+        elif CTTXW == "3":
+            while True:
+                DTRser = input("\033[0;32;40m[INFO]\033[0m 请输入DTR端口：")
+                DTRrate = int(input("\033[0;32;40m[INFO]\033[0m 请输入DTR波特率："))
+                try:
+                    ser3 = serial.Serial(DTRser, DTRrate, timeout=0.5)
+                    ser3.setRTS(False)
+                    ser3.setDTR(False)
+                    break
+                except:
+                    print("\033[0;31;40m[ERRO]\033[0m 串口打开失败")
+                    input("\033[0;31;40m[ERRO]\033[0m 请确保DTR串口设置正确"
+                          "短按Enter键进行重新设置")
+        else:
+            input('\033[0;31;40m[ERRO]\033[0m ')
+            break
         print("\033[0;32;40m[INFO]\033[0m 发射控制子菜单：")
         print("\033[0;32;40m[INFO]\033[0m 短按Enter开始TX，再按一次结束TX")
         print("\033[0;32;40m[INFO]\033[0m 输入Q退出")
@@ -324,7 +452,14 @@ while True:
                     print("\033[0;32;40m[INFO]\033[0m 发射控制子菜单：")
                     print("\033[0;32;40m[INFO]\033[0m 短按Enter开始TX，再按一次结束TX")
                     print("\033[0;32;40m[INFO]\033[0m 输入Q退出")
-                    ser.write(bytes.fromhex("0000000008"))
+                    if CTTXW == "1":
+                        ser.write(bytes.fromhex("0000000008"))
+                    elif CTTXW == "2":
+                        ser3.setRTS(True)
+                    else:
+                        ser3.setDTR(True)
+
+
                     print("\033[0;32;40m[INFO]\033[0m 当前状态：TX")
                     Tx = 1
                 else:
@@ -332,7 +467,12 @@ while True:
                     print("\033[0;32;40m[INFO]\033[0m 发射控制子菜单：")
                     print("\033[0;32;40m[INFO]\033[0m 短按Enter开始TX，再按一次结束TX")
                     print("\033[0;32;40m[INFO]\033[0m 输入Q退出")
-                    ser.write(bytes.fromhex("0000000088"))
+                    if CTTXW == "1":
+                        ser.write(bytes.fromhex("0000000088"))
+                    elif CTTXW == "2":
+                        ser3.setRTS(False)
+                    else:
+                        ser3.setDTR(True)
                     print("\033[0;32;40m[INFO]\033[0m 当前状态：RX")
                     Tx = 0
             elif key == "Q" or "q":  # 退出发射控制子菜单
